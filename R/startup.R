2860e9bacd37947f432f55f1af6c7606d0e0b5e3
Sasha D. Hafner <sasha@hafnerconsulting.com>
2026-02-25 16:18:34 -0500

    fix startup.
    Problem was (at least one of them was) that xa_init is now grouped in conc_init

--- R/startup.R
+++ R/startup.R
@@ -48,7 +48,10 @@ abm_startup <- function(
 starting_pars <- function(pars, starting) {
   
   message('Using starting conditions from `starting` argument')
-  pars$xa_init[pars$grps] <- as.numeric(starting[nrow(starting), paste0(pars$grps, '_conc')])
+  pars$conc_init[pars$grps] <- as.numeric(starting[nrow(starting), paste0(pars$grps, '_conc')])
+  # Remember xa_init and other stuff is grouped into conc_init, so next line not needed
+  # Potential source of confusion!
+  #pars$xa_init[pars$grps] <- as.numeric(starting[nrow(starting), paste0(pars$grps, '_conc')])
   pars$conc_init['CH3COOH'] <- as.numeric(starting[nrow(starting), 'CH3COOH_conc'])
   pars$sub_init[pars$subs] <- as.numeric(starting[nrow(starting), paste0(pars$subs, '_conc')])
   # Set slurry_mass as well?

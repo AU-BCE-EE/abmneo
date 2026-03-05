43130ecb98d200af3da785d445aa749d432f581c
Sasha D. Hafner <sasha@hafnerconsulting.com>
2026-03-05 15:43:31 -0500

    finish rm stoich calcs

--- R/stoich.R
+++ R/stoich.R
@@ -1,47 +1,5 @@
 # Stoichometry functions
 
-mol_mass <- function(form, elements = NULL) {
-
-  ## Check argument
-  #checkArgClassValue(form, 'character')
-
-  # Loop through all elements in form
-  mmass <- NULL
-  for(f in form) {
-    # If and only if first letter of form is lowercase, entire string is capitalized
-    if(grepl('^[a-z]', f)) f <- toupper(f) 
-
-    # Get coefficients of formula
-    fc <- read_formula(f)
-
-    if (!is.null(elements)) {
-      fc <- fc[intersect(names(fc), elements)]
-    }
-
-    # Check for unidentified element
-    if(any(!names(fc) %in% names(atom.weights))) stop('One or more elements in \"form\" is not in the database. You can add it to the \"atom.weights\" vector if you want to modify the function code. Otherwise send a request to sasha.hafner@bce.au.dk.')
-
-    # Calculate molar mass, using names of fc for indexing
-    mmass <- c(mmass, sum(atom.weights[names(fc)]*fc))
-  }
-
-  return(mmass)
-}
-
-# Returns COD per mol substrate
-calc_COD <- function(form) {
-
-  # If and only if first letter of form is lowercase, entire string is capitalized
-  if(grepl('^[a-z]', form)) form <- toupper(form)
-  # Read formula (function not vectorized)
-  fc <- read_formula(form, elements = c('C', 'H', 'O', 'N'))
-  # Calculate COD based on Rittmann and McCarty
-  COD <- as.vector((2*fc['C'] + 0.5*fc['H'] - 1.5*fc['N'] - fc['O']) * mol_mass('O'))
-
-  return(COD)
-}
-
-
 # Get mass conversion factor to go from moles of component to mass COD, N, C, S, or total, in that order
 get_mass_conv <- function(form) {
 
